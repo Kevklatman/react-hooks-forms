1f7f1616f77586835693bef478b787bb2dc6390e
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-07 22:29:51 -0600

    l

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,20 @@
+// src/components/Form
+import React, { useState } from "react";
+import Form from "./Form";
+import DisplayData from "./DisplayData";
+
+function ParentComponent() {
+  return (
+    <div>
+      <Form
+        firstName={firstName}
+        lastName={lastName}
+        handleFirstNameChange={handleFirstNameChange}
+        handleLastNameChange={handleLastNameChange}
+      />
+      <DisplayData firstName={firstName} lastName={lastName} />
+    </div>
+  );
+}
+
+export default ParentComponent;
